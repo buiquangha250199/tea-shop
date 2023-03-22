918f059b6d73c2fa4861a794d5ac91293098ecad
Bui Quang Ha <buiquangha250199@gmail.com>
2023-03-22 20:57:20 +0700

    :+1: First commit with homepage

---
+++ layouts/default.vue
@@ -0,0 +1,624 @@
+<template>
+  <v-app dark class="default-layout">
+    <v-navigation-drawer v-model="drawer" absolute temporary app>
+      <v-list dense>
+        <v-list-item-group color="primary">
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-content>
+              <v-list-item-title class="text-subtitle-2">{{
+                item.text
+              }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-system-bar
+      ref="system-bar"
+      :class="['top-header', fixed ? '-fixed elevation-3' : '']"
+      :fixed="fixed"
+      transition="scroll-y-transition"
+    >
+      <img class="app-logo" src="~/assets/img/logo/app-logo.png" />
+      <div class="search">
+        <v-text-field
+          label="Tìm sản phẩm..."
+          placeholder="Tìm sản phẩm..."
+          large
+          solo
+          clearable
+          hide-details="auto"
+          class="input"
+        ></v-text-field>
+        <v-btn class="btn" color="green">
+          <span class="mdi mdi-magnify"></span>
+        </v-btn>
+      </div>
+      <div class="text">
+        <div class="icon">
+          <span class="mdi mdi-heart-circle"></span>
+          Yêu thích
+        </div>
+        <div class="icon">
+          <span class="mdi mdi-truck"></span>
+          Mua sỉ
+        </div>
+      </div>
+    </v-system-bar>
+    <v-app-bar class="nav-bar" :clipped-left="clipped" absolute app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-row align="center" class="app-link">
+        <v-menu offset-y>
+          <template #activator="{ on, attrs }">
+            <v-btn
+              class="menubtn"
+              color="green"
+              text
+              x-large
+              v-bind="attrs"
+              v-on="on"
+            >
+              <span class="mdi mdi-menu-down text-large"></span>
+              Danh mục
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item
+              v-for="(item, index) in productCategories"
+              :key="index"
+              :to="item.to"
+            >
+              <v-list-item-title
+                ><span class="mdi mdi-menu-right"></span
+                >{{ item.text }}</v-list-item-title
+              >
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <v-btn color="green" text x-large> Sản phẩm </v-btn>
+        <v-btn color="green" text x-large> Tin tức </v-btn>
+        <v-btn color="green" text x-large> Liên hệ </v-btn>
+      </v-row>
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer class="layout-footer" :absolute="true" app>
+      <v-row justify="center" class="message main-container">
+        <v-col cols="12" sm="4" class="text-large">
+          <div class="flex-center gap-3">
+            <span class="icon mdi mdi-thumb-up-outline"></span>
+            <div class="content">
+              <div class="title mb-2">Uy tín hàng đầu</div>
+              <div class="text-normal text-body-2">
+                Thành lập hơn 5 Năm, đã phục vụ hơn 300.000 khách hàng trên cả
+                nước...
+              </div>
+            </div>
+          </div>
+        </v-col>
+        <v-col cols="12" sm="4" class="text-large">
+          <div class="flex-center gap-3">
+            <span class="icon mdi mdi-truck-delivery"></span>
+            <div class="content">
+              <div class="title mb-2">Giao hàng nhanh chóng</div>
+              <div class="text-normal text-body-2">
+                Giao hàng 24h trong nội thành và giao hàng qua bưu điện ở tỉnh
+                khác...
+              </div>
+            </div>
+          </div>
+        </v-col>
+        <v-col cols="12" sm="4" class="text-large">
+          <div class="flex-center gap-3">
+            <span class="icon mdi mdi-tea-outline"></span>
+            <div class="content">
+              <div class="title mb-2">Sản phẩm đa dạng</div>
+              <div class="text-normal text-body-2">
+                Cung cấp tất cả các loại tinh bột, thảo dược, bột trà xanh, mầm
+                đậu nành...
+              </div>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+      <v-row justify="center" class="contact main-container">
+        <v-col cols="12" sm="6" class="text-large">
+          <div class="content">
+            <div class="text-subtitle-2 mb-2">LIÊN HỆ</div>
+            <div class="text-h6 font-bold mb-2">TRÀ HOA NHÀ MỘC</div>
+            <div class="text-normal mb-2">
+              <span class="font-bold">Địa chỉ: </span>505 Trần Khát Chân – Thanh
+              Nhàn – HBT – HN
+            </div>
+            <div class="text-normal mb-2">
+              <span class="font-bold">Số điện thoại: </span>0923.456.789
+            </div>
+          </div>
+        </v-col>
+        <v-col cols="12" sm="6" class="text-large">
+          <v-row>
+            <v-col cols="6" sm="6">
+              <div class="text-subtitle-2 mb-2">VỀ CHÚNG TÔI</div>
+              <v-list class="about-list">
+                <v-list-item
+                  v-for="(item, index) in aboutList"
+                  :key="index"
+                  :to="item.to"
+                  class="padding-0 height-normal item"
+                >
+                  <v-list-item-title class="text-color-main text-normal"
+                    ><span class="mdi mdi-menu-right"></span
+                    >{{ item.text }}</v-list-item-title
+                  >
+                </v-list-item>
+              </v-list>
+            </v-col>
+            <v-col cols="6" sm="6">
+              <div class="text-subtitle-2 mb-2">CHÍNH SÁCH</div>
+              <v-list class="about-list">
+                <v-list-item
+                  v-for="(item, index) in policies"
+                  :key="index"
+                  :to="item.to"
+                  class="padding-0 height-normal item"
+                >
+                  <v-list-item-title class="text-color-main text-normal"
+                    ><span class="mdi mdi-menu-right"></span
+                    >{{ item.text }}</v-list-item-title
+                  >
+                </v-list-item>
+              </v-list>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row justify="center" class="other main-container w-full">
+        <v-col cols="12" sm="4" class="text-center">
+          <div class="text-subtitle-2">PHƯƠNG THỨC THANH TOÁN</div>
+          <v-row>
+            <v-col cols="2">
+              <img src="~/assets/img/pay-5.png" />
+            </v-col>
+            <v-col cols="2">
+              <img src="~/assets/img/pay-1.png" />
+            </v-col>
+            <v-col cols="2">
+              <img src="~/assets/img/pay-2.png" />
+            </v-col>
+            <v-col cols="2">
+              <img src="~/assets/img/pay-3.png" />
+            </v-col>
+            <v-col cols="2">
+              <img src="~/assets/img/pay-4.png" />
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="12" sm="4" class="text-center">
+          <div class="text-subtitle-2">DỊCH VỤ GIAO HÀNG</div>
+          <v-row>
+            <v-col cols="6">
+              <img src="~/assets/img/logo-giao-hang-nhanh.png" />
+            </v-col>
+            <v-col cols="6">
+              <img src="~/assets/img/logo-vietnam-post.png" />
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="12" sm="4" class="text-center">
+          <div class="text-subtitle-2 mb-2">KÊNH TRUYỀN THÔNG</div>
+          <v-row justify="center" class="gap-3 social-media">
+            <nuxt-link to="/" target="_blank">
+              <span class="mdi mdi-facebook text-large item"></span>
+            </nuxt-link>
+            <nuxt-link to="/">
+              <span class="mdi mdi-twitter text-large item"></span>
+            </nuxt-link>
+            <nuxt-link to="/">
+              <span class="mdi mdi-google-plus text-large item"></span>
+            </nuxt-link>
+            <nuxt-link to="/">
+              <span class="mdi mdi-youtube text-large item"></span>
+            </nuxt-link>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row class="flex-center">
+        <v-col cols="12">
+          <p class="text-normal text-center">
+            © 2019 của Trà Hoa Nhà Mộc <br />Giấy chứng nhận cơ sở đủ điều kiện
+            an toàn thực phẩm<br />Sản Phẩm không phải đúng với tất cả mọi
+            người, tùy vào mỗi cơ địa sẽ có những mức tác dụng khác nhau<br />Nội
+            dung được đăng tải lên website chỉ mang tính chất tham khảo
+          </p>
+        </v-col>
+      </v-row>
+      <a class="hotline" title="hotline" href="tel:0923456789">
+        <span class="pc">Hotline 092.345.6789</span>
+        <span class="mdi mdi-phone mobile text-normal">Hotline</span>
+      </a>
+      <a
+        class="zalo"
+        title="zalo"
+        href="https://zalo.me/0923456789"
+        target="_blank"
+      >
+        <span class="pc">Zalo 092.345.6789</span>
+        <span class="mdi mdi-phone mobile text-normal">Zalo</span>
+      </a>
+      <div class="pagetop" @click="scrollToTop">
+        <span class="mdi mdi-arrow-up-bold text-large btn"></span>
+      </div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          text: 'Chính sách bảo mật',
+        },
+        {
+          text: 'Chính sách đại lý',
+        },
+        {
+          text: 'Chính sách đổi trả',
+        },
+        {
+          text: 'Chính sách vận chuyển',
+        },
+        {
+          text: 'Đặt hàng thành công',
+        },
+        {
+          text: 'Điều khoản và điều kiện',
+        },
+        {
+          text: 'Giỏ hàng',
+        },
+        {
+          text: 'Giới thiệu',
+        },
+        {
+          text: 'Hệ thống đại lý',
+        },
+        {
+          text: 'Hướng dẫn mua hàng',
+        },
+        {
+          text: 'Hướng dẫn thanh toán',
+        },
+        {
+          text: 'Liên Hệ',
+        },
+        {
+          text: 'Sản phẩm',
+        },
+        {
+          text: 'Tài khoản',
+        },
+        {
+          text: 'Tầm nhìn chiến lược',
+        },
+        {
+          text: 'Thanh toán',
+        },
+        {
+          text: 'Theo dõi đơn hàng',
+        },
+        {
+          text: 'Hệ thống đại lý',
+        },
+        {
+          text: 'Triết lý kinh doanh',
+        },
+        {
+          text: 'Tuyển đại lý - CTV',
+        },
+      ],
+      productCategories: [
+        { text: 'Trà Mix Vị', to: '/' },
+        { text: 'Sản phẩm trà hoa – Quả khô', to: '/' },
+        { text: 'Trà thảo dược', to: '/' },
+        { text: 'Thực phẩm dinh dưỡng', to: '/' },
+        { text: 'Trà Trị Liệu', to: '/' },
+      ],
+      aboutList: [
+        {
+          text: 'Giới thiệu',
+          to: '/',
+        },
+        {
+          text: 'Triết lý kinh doanh',
+          to: '/',
+        },
+        {
+          text: 'Tầm nhìn chiến lược',
+          to: '/',
+        },
+        {
+          text: 'Liên hệ',
+          to: '/',
+        },
+        {
+          text: 'Ý kiến khách hàng',
+          to: '/',
+        },
+        {
+          text: 'Hoạt động công ty',
+          to: '/',
+        },
+      ],
+      policies: [
+        {
+          text: 'Hướng dẫn mua hàng',
+          to: '/',
+        },
+        {
+          text: 'Hướng dẫn thanh toán',
+          to: '/',
+        },
+        {
+          text: 'Chính sách vận chuyển',
+          to: '/',
+        },
+        {
+          text: 'Chính sách đổi trả',
+          to: '/',
+        },
+        {
+          text: 'Chính sách bảo mật',
+          to: '/',
+        },
+        {
+          text: 'Điều khoản và điều kiện',
+          to: '/',
+        },
+      ],
+      miniVariant: false,
+    }
+  },
+  beforeMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  unmounted() {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll() {
+      if (window.scrollY > 160) {
+        this.fixed = true
+      } else this.fixed = false
+    },
+    scrollToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      })
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+.top-header {
+  height: 100px !important;
+  padding: 0px 10%;
+  background-color: #f5f5f5;
+  display: flex;
+  z-index: 4;
+  &.-fixed {
+    border-bottom: 1px solid #ececec;
+    animation: stuckMoveDown 0.6s;
+  }
+  > .text {
+    display: flex;
+    justify-content: center;
+    gap: 32px;
+    color: $text-color;
+  }
+  > .text > .icon {
+    display: flex;
+    justify-content: center;
+    font-size: 24px;
+    gap: 4px;
+  }
+  > .search {
+    display: flex;
+    justify-content: center;
+  }
+  > .search > .input {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+  > .search > .btn {
+    height: 48px;
+    width: 48px;
+    color: #fff;
+    font-size: 24px;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    min-width: unset;
+  }
+}
+.nav-bar {
+  top: 90px;
+  margin-top: 0;
+}
+
+.app-link {
+  gap: 12px;
+  margin-left: 12px;
+}
+.about-list {
+  .item {
+    min-height: 36px !important;
+    &:hover {
+      text-decoration: underline;
+      text-decoration-color: $text-color;
+    }
+  }
+}
+
+.layout-footer {
+  .icon {
+    padding: 14px 20px;
+    border: 1px solid $text-color;
+    border-radius: 60%;
+    color: $text-color;
+  }
+  .message {
+    border-top: 1px solid #ececec;
+    border-bottom: 1px solid #ececec;
+    background-color: #fff;
+    padding-top: 5px;
+    padding-bottom: 5px;
+  }
+  .contact {
+    border-bottom: 1px solid #ececec;
+    background-color: #fff;
+    padding-top: 5px;
+    padding-bottom: 5px;
+  }
+  .other {
+    border-bottom: 1px solid #ececec;
+    padding-top: 5px;
+    padding-bottom: 5px;
+  }
+  .pagetop {
+    position: fixed;
+    right: 20px;
+    bottom: 130px;
+  }
+  .pagetop > .btn {
+    padding: 3px 4px;
+    border: 1px solid $text-color;
+    cursor: pointer;
+    border-radius: 10px;
+    background-color: $text-color;
+    color: #fff;
+    opacity: 0.7;
+    &:hover {
+      opacity: 1;
+    }
+  }
+  .hotline {
+    position: fixed;
+    right: 10px;
+    bottom: 10px;
+    font-size: 20px;
+    color: #e53935;
+    border: 1px dotted #e53935;
+    border-radius: 30px;
+    text-decoration: none;
+    padding: 5px 12px;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+  .zalo {
+    position: fixed;
+    right: 10px;
+    bottom: 70px;
+    font-size: 20px;
+    color: #1e88e5;
+    border: 1px dotted #1e88e5;
+    border-radius: 30px;
+    text-decoration: none;
+    padding: 5px 12px;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+}
+.social-media {
+  .item {
+    transition: 0.3s ease-in-out;
+    display: block;
+    &:hover {
+      transform: translate(0, -5px);
+    }
+  }
+}
+@media only screen and (min-width: 768px) {
+  .app-logo {
+    width: 175px;
+    height: auto;
+  }
+  .top-header {
+    > .search {
+      width: 55%;
+      margin-right: 3%;
+    }
+  }
+}
+
+@media only screen and (min-width: 768px) and (max-width: 1025px) {
+  .top-header {
+    > .search {
+      width: 75%;
+      margin-right: 5%;
+    }
+    > .text {
+      display: none;
+    }
+  }
+}
+@media only screen and (max-width: 768px) {
+  .app-logo {
+    width: 120px;
+    height: auto;
+    margin-right: -25px;
+    margin-left: -25px;
+  }
+  .top-header {
+    gap: 5%;
+    > .text {
+      display: none;
+    }
+    > .search {
+      font-size: 12px;
+    }
+  }
+  .app-link {
+    gap: 6px;
+    > .v-btn {
+      font-size: 12px;
+      width: 20%;
+      min-width: unset;
+    }
+    > .menubtn {
+      margin-right: 20px;
+    }
+  }
+  .layout-footer {
+    .zalo {
+      bottom: 50px;
+    }
+    .pagetop {
+      bottom: 92px;
+    }
+    .pagetop > .btn {
+      padding: 5px;
+    }
+  }
+}
+@keyframes stuckMoveDown {
+  0% {
+    transform: translateY(-100%);
+  }
+
+  100% {
+    transform: translateY(0);
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,274 @@
+<template>
+  <v-row justify="center" class="mb-8">
+    <v-col cols="12" sm="9">
+      <v-carousel
+        cycle
+        interval="3000"
+        hide-delimiter-background
+        show-arrows-on-hover
+        delimiter-icon="mdi-minus"
+      >
+        <v-carousel-item>
+          <img class="banner-img" src="~/assets/img/banner/img-1.png" />
+        </v-carousel-item>
+        <v-carousel-item>
+          <img class="banner-img" src="~/assets/img/banner/img-2.png" />
+        </v-carousel-item>
+        <v-carousel-item>
+          <img class="banner-img" src="~/assets/img/banner/img-3.png" />
+        </v-carousel-item>
+        <v-carousel-item>
+          <img class="banner-img" src="~/assets/img/banner/img-4.png" />
+        </v-carousel-item>
+      </v-carousel>
+    </v-col>
+    <v-col cols="12" sm="3">
+      <v-row>
+        <v-col cols="12" sm="12">
+          <v-card class="contact-card">
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title class="text-h6"> Hợp Tác </v-card-title>
+                <v-card-subtitle class="subtitle"
+                  >PHÂN PHỐI - ĐẠI LÝ</v-card-subtitle
+                >
+                <v-card-actions>
+                  <v-btn class="ms-2" variant="outlined" size="small">
+                    LIÊN HỆ
+                  </v-btn>
+                </v-card-actions>
+              </div>
+              <v-avatar class="ma-3" size="70" rounded="0">
+                <img src="~/assets/img/logo/app-logo.png" />
+              </v-avatar>
+            </div>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="12">
+          <v-card theme="dark" class="contact-card">
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title class="text-h6"> Báo Giá Sỉ </v-card-title>
+                <v-card-subtitle class="subtitle"
+                  >TRÀ - HOA, QUẢ KHÔ</v-card-subtitle
+                >
+                <v-card-actions>
+                  <v-btn class="ms-2" variant="outlined" size="small">
+                    LIÊN HỆ
+                  </v-btn>
+                </v-card-actions>
+              </div>
+              <v-avatar class="ma-3" size="70" rounded="0">
+                <img src="~/assets/img/logo/app-logo.png" />
+              </v-avatar>
+            </div>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="12">
+          <v-card theme="dark" class="contact-card">
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title class="text-h6"> Báo Giá Sỉ </v-card-title>
+                <v-card-subtitle class="subtitle"
+                  >TINH BỘT - TINH DẦU</v-card-subtitle
+                >
+                <v-card-actions>
+                  <v-btn class="ms-2" variant="outlined" size="small">
+                    LIÊN HỆ
+                  </v-btn>
+                </v-card-actions>
+              </div>
+              <v-avatar class="ma-3" size="70" rounded="0">
+                <img src="~/assets/img/logo/app-logo.png" />
+              </v-avatar>
+            </div>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="12">
+          <v-card theme="dark" class="contact-card">
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title class="text-h6"> Dịch Vụ </v-card-title>
+                <v-card-subtitle class="subtitle -last"
+                  >NGHIỀN - SẤY
+                  <span class="sp">- VIÊN HOÀN</span></v-card-subtitle
+                >
+                <v-card-actions>
+                  <v-btn class="ms-2" variant="outlined" size="small">
+                    LIÊN HỆ
+                  </v-btn>
+                </v-card-actions>
+              </div>
+              <v-avatar class="ma-3" size="70" rounded="0">
+                <img src="~/assets/img/logo/app-logo.png" />
+              </v-avatar>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      images: ['img-1.png', 'img-2.png', 'img-3.png', 'img-4.png'],
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+.contact-card {
+  background-image: url('~/assets/img/bg_card.png');
+  background-size: 70px 100%;
+  position: relative;
+  padding: 6.2px 0;
+  img {
+    position: absolute;
+    width: 100px;
+    height: 100px;
+  }
+  .text-h6 {
+    font-size: 1em !important;
+    margin-top: -15px;
+    color: crimson;
+    margin-left: 58px;
+    font-size: 1.1em !important;
+  }
+  .subtitle {
+    font-size: 12px;
+    padding-right: 0;
+    overflow: overlay;
+    color: #0288d1;
+    font-weight: 700;
+    margin-left: 34px;
+    &.-last {
+      margin-left: 7px;
+    }
+  }
+  .v-card__actions {
+    position: absolute;
+    top: 56px;
+    left: 48px;
+  }
+  .v-btn {
+    background-color: #1c87c9;
+    border-radius: 60px;
+    border: none;
+    color: #eeeeee;
+    cursor: pointer;
+    display: inline-block;
+    font-family: sans-serif;
+    font-size: 13px;
+    padding: 5px 15px;
+    text-align: center;
+    text-decoration: none;
+    animation: glowing 1300ms infinite;
+  }
+}
+.v-carousel__item {
+  height: auto !important;
+}
+.banner-img {
+  max-height: 500px;
+  width: auto;
+  object-fit: cover;
+}
+@media only screen and(min-width: 768px) and (max-width: 850px) {
+  .contact-card {
+    img {
+      display: none;
+    }
+  }
+}
+@media only screen and(min-width: 768px) and (max-width: 1025px) {
+  .sp {
+    display: none;
+  }
+  .contact-card {
+    .text-h6 {
+      position: absolute;
+      left: 30px;
+      margin-left: 0;
+    }
+    img {
+      width: 70px;
+      height: 70px;
+      margin-left: 20px;
+    }
+    .v-card__actions {
+      left: 22px;
+      top: 56px;
+    }
+    .subtitle {
+      position: absolute;
+      top: 52px;
+      left: 7px;
+      margin-top: -13px;
+      margin-left: 0;
+      &.-last {
+        margin-left: 15px;
+      }
+    }
+  }
+}
+@media only screen and(min-width: 630px) and (max-width: 767px) {
+  .contact-card {
+    img {
+      display: none;
+    }
+  }
+  .v-carousel__item {
+    height: 200px !important;
+  }
+  .banner-img {
+    max-height: 200px;
+    width: auto;
+    object-fit: cover;
+  }
+}
+@media only screen and (max-width: 629px) {
+  .contact-card {
+    img {
+      right: -5px;
+      height: 100px;
+      width: 100px;
+    }
+    .subtitle {
+      position: absolute;
+      left: 38px;
+      top: 54px;
+    }
+    .text-h6 {
+      position: absolute;
+      left: 34px;
+    }
+    .v-card__actions {
+      left: 80px;
+    }
+  }
+  /* .banner-img {
+    max-height: 215px;
+  }
+  .v-carousel {
+    max-height: 225px !important;
+  } */
+}
+@keyframes glowing {
+  0% {
+    background-color: #2ba805;
+    box-shadow: 0 0 5px #2ba805;
+  }
+  50% {
+    background-color: #49e819;
+    box-shadow: 0 0 20px #49e819;
+  }
+  100% {
+    background-color: #2ba805;
+    box-shadow: 0 0 5px #2ba805;
+  }
+}
+</style>
